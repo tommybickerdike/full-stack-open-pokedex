b3c4e0d4951b7a15af0bf63014ece4b39de02340
Tommy <tommy@musicglue.com>
2021-12-09 11:03:28 +0000

    11.9

---
+++ cypress/integration/Pokedex.js
@@ -0,0 +1,18 @@
+
+describe('Pokedex', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5000/')
+  })
+
+  it('front page can be opened', () => {
+    cy.get('#app').should('be.visible')
+		cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated to', () => {
+    cy.contains('charmander').click()
+		cy.get('.pokemon-page').should('contain', 'charmander')
+  })
+
+})
